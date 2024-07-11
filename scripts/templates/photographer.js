56a75ca14d9208b61723dbce373fb40e33d3cbe4
Iana <iana.li.timofeevna@gmail.com>
2024-07-11 12:01:32 +0200

    [add fetch and create the cards with info]

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,56 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, portrait, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+
+        let cardHtmlModel = `
+                        <article class="card-wrapper">
+                            <div class="card">
+                                <a href='photographer.html?id=${id}&name=${name}' class="profile-link">
+                                <img src="media/img/PhotographersIDPhotos/${portrait}" class="profile-img-big">
+                                <h2 class="profile-card-title">${name}</h2>
+                                </a>
+                            <div class="card-text">
+                                <p class="profile-loc-text">${city}, ${country}</p>
+                                <p class="profile-tagline">${tagline}</p>
+                                <p class="profile-price">${price}€/jour</p>
+                            </div>
+                            </div>
+                        </article>`;
+
+        //create article
+
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographersSection.insertAdjacentHTML('beforebegin', cardHtmlModel)
+
+        // //create img and add src
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", portrait)
+
+        // //create html elements 
+        // const h2 = document.createElement( 'h2' );
+        // const cityCard = document.createElement( 'h4' )
+        // const countryCard = document.createElement( 'h4' )
+
+        // //setAttribute for html elements
+        // cityCard.setAttribute('class', 'card-location');
+        // countryCard.setAttribute('class', 'card-location');
+
+        // //add meaning 
+        // h2.textContent = name;
+        // cityCard.textContent = city;
+        // countryCard.textContent = country;
+
+        // //add elements to html
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        // article.appendChild(cityCard)
+        // article.appendChild(cardHtmlModel)
+
+        // //return article
+        // return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, city, country, portrait, tagline, price, getUserCardDOM }
 }
